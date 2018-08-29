20955eb4d3d794cfd1e479bdb9857783532553cb
Sara Muntean <munteansr@gmail.com>
2018-08-29 09:16:38 -0400

    Revised data object that is passed into fetch request. Added console.logs for debugging. Still not working.

--- public/js/login.js
+++ public/js/login.js
@@ -10,21 +10,24 @@ let loginForm = document.querySelector('.login-form');
 let createUser = (event) => {
   event.preventDefault();
   let email = document.querySelector('.login-email').value;
+  console.log(email);
   let password = document.querySelector('.login-password').value;
+  console.log(password);
+  let data ={emai: email, user_password: password};
   fetch('http://localhost:3000/users', {
     method: "POST", 
         mode: "no-cors",
         cache: "no-cache", 
         credentials: "same-origin", 
         headers: {
-          'Content-Type': 'application/json'
+          "content-type": "application/json"
         },
         redirect: "follow", 
         referrer: "no-referrer", 
-        body: JSON.stringify({email: email, user_password: password}),
+        body: JSON.stringify(data),
   })
   .then(response => {
-    console.log(response);
+      console.log(response);
   })
 }
 
