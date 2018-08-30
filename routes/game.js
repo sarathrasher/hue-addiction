ad2a92505a0b832d159e18684d0f6fda14a1052b
Ian Gornall <ian.gornall@gmail.com>
2018-08-30 16:18:42 -0400

    Formatted data with average.

--- routes/game.js
+++ routes/game.js
@@ -1,28 +1,50 @@
 const db = require('../database');
 
-let formatData = data => {
+let formatUserData = data => {
   let formattedData = {};
-  console.log('Formatting data...')
   for (entry of data) {
-    console.log(entry);
     if (!formattedData['stage' + entry.stage]) {
       formattedData['stage' + entry.stage] = {};
     }
     if (!formattedData['stage' + entry.stage][entry.level]) {
       formattedData['stage' + entry.stage][entry.level] = {};
-      formattedData['stage' + entry.stage][entry.level].user = {};
     }
-    formattedData['stage' + entry.stage][entry.level].user.score = entry.score;
-    formattedData['stage' + entry.stage][entry.level].user.time = entry.level_time;
+    formattedData['stage' + entry.stage][entry.level].score = entry.score;
+    formattedData['stage' + entry.stage][entry.level].time = entry.level_time;
+  }
+  return formattedData;
+}
+
+let formatAverageData = data => {
+  let formattedData = {};
+  for (entry of data) {
+    if (!formattedData['stage' + entry.stage]) {
+      formattedData['stage' + entry.stage] = {};
+    }
+    if (!formattedData['stage' + entry.stage][entry.level]) {
+      formattedData['stage' + entry.stage][entry.level] = {};
+      formattedData['stage' + entry.stage][entry.level].score = entry.score;
+      formattedData['stage' + entry.stage][entry.level].time = entry.level_time;
+      formattedData['stage' + entry.stage][entry.level].num_users = 1;
+    } else {
+      formattedData['stage' + entry.stage][entry.level].num_users++;
+      formattedData['stage' + entry.stage][entry.level].score = (formattedData['stage' + entry.stage][entry.level].score + entry.score) / formattedData['stage' + entry.stage][entry.level].num_users;
+      formattedData['stage' + entry.stage][entry.level].time = (formattedData['stage' + entry.stage][entry.level].score + entry.level_time) / formattedData['stage' + entry.stage][entry.level].num_users;
+    }
   }
   return formattedData;
 }
 
 let getGameData = (req, res) => {
   console.log(req.user.id);
-  db.query(`SELECT stage, level, score, level_time FROM game WHERE user_id = ${req.user.id}`)
+  let userData = averageData = db.query(`SELECT stage, level, score, level_time FROM game WHERE user_id = ${req.user.id}`);
+  let allData = db.query(`SELECT stage, level, score, level_time FROM game`);
+  Promise.all([userData, allData])
   .then(data => {
-    res.send(formatData(data));
+    console.log(data);
+    let userData = formatUserData(data[0]);
+    let averageData = formatAverageData(data[1]);
+    res.send({user: userData, average: averageData});
   })
   .catch(err => {
     res.send(err);
