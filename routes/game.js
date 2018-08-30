a06446e70d2194da8a0bce781156a8b0b00f674c
Ian Gornall <ian.gornall@gmail.com>
2018-08-30 15:11:12 -0400

    Added route to get game data.

--- routes/game.js
+++ routes/game.js
@@ -3,27 +3,26 @@ const db = require('../database');
 let formatData = (userId, data) => {
   let formattedData = {};
   for (entry of data) {
-    if (entry[user_id] === userId) {
-      if (!formattedData['stage' + entry.stage]) {
-        formattedData['stage' + entry.stage] = {};
-      }
-      if (!formattedData['stage' + entry.stage][entry.level]) {
-        formattedData['stage' + entry.stage][entry.level] = {};
-        formattedData['stage' + entry.stage][entry.level].user = {};
-      }
-      formattedData['stage' + entry.stage][entry.level].user.score = entry.score;
-      formattedData['stage' + entry.stage][entry.level].user.time = entry.level_time;
+    console.log(entry, userId);
+    if (!formattedData['stage' + entry.stage]) {
+      formattedData['stage' + entry.stage] = {};
     }
+    if (!formattedData['stage' + entry.stage][entry.level]) {
+      formattedData['stage' + entry.stage][entry.level] = {};
+    }
+    if (!formattedData['stage' + entry.stage][entry.level][entry.user_id]) {
+      formattedData['stage' + entry.stage][entry.level][entry.user_id] = {};
+    }
+    formattedData['stage' + entry.stage][entry.level][entry.user_id].score = entry.score;
+    formattedData['stage' + entry.stage][entry.level][entry.user_id].time = entry.level_time;
   }
   return formattedData;
 }
 
 let getGameData = (req, res) => {
-  console.log('User: ' + req.user.id);
-  let game_data = {};
   db.query(`SELECT user_id, stage, level, score, level_time FROM game`)
   .then(data => {
-    res.send(formatData(req.user.id, data));
+    res.send(formatData(data));
   })
   .catch(err => {
     res.send(err);
@@ -31,6 +30,7 @@ let getGameData = (req, res) => {
 }
 
 let postGameData = (req, res) => {
+  console.log('User: ' + req.user.id);
   console.log(req.user);
 }
 
