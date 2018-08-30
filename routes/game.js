f47c6ab11ac8f9c25ff1402910e0622c7e25c998
Ian Gornall <ian.gornall@gmail.com>
2018-08-30 15:42:42 -0400

    Sending current user data only.

--- routes/game.js
+++ routes/game.js
@@ -1,26 +1,26 @@
 const db = require('../database');
 
-let formatData = (userId, data) => {
+let formatData = data => {
   let formattedData = {};
+  console.log('Formatting data...')
   for (entry of data) {
-    console.log(entry, userId);
+    console.log(entry);
     if (!formattedData['stage' + entry.stage]) {
       formattedData['stage' + entry.stage] = {};
     }
     if (!formattedData['stage' + entry.stage][entry.level]) {
       formattedData['stage' + entry.stage][entry.level] = {};
+      formattedData['stage' + entry.stage][entry.level].user = {};
     }
-    if (!formattedData['stage' + entry.stage][entry.level][entry.user_id]) {
-      formattedData['stage' + entry.stage][entry.level][entry.user_id] = {};
-    }
-    formattedData['stage' + entry.stage][entry.level][entry.user_id].score = entry.score;
-    formattedData['stage' + entry.stage][entry.level][entry.user_id].time = entry.level_time;
+    formattedData['stage' + entry.stage][entry.level].user.score = entry.score;
+    formattedData['stage' + entry.stage][entry.level].user.time = entry.level_time;
   }
   return formattedData;
 }
 
 let getGameData = (req, res) => {
-  db.query(`SELECT user_id, stage, level, score, level_time FROM game`)
+  console.log(req.user.id);
+  db.query(`SELECT stage, level, score, level_time FROM game WHERE user_id = ${req.user.id}`)
   .then(data => {
     res.send(formatData(data));
   })
