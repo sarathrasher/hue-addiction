96b91d4f9f6fb00851ce148dbbfef47595f7f99b
Sara Muntean <munteansr@gmail.com>
2018-08-31 12:09:51 -0400

    Grabbed data from fetch request and populated the page with score data whenever game is finished/level button is clicked

--- public/js/stats.js
+++ public/js/stats.js
@@ -1,8 +1,39 @@
 let personalScore = document.querySelector('.personal-score');
 let personalTime = document.querySelector('.personal-time');
 let averageScore = document.querySelector('.average-score');
-let averageTime = document.querySelector('.average-item');
+let averageTime = document.querySelector('.average-time');
+let levelLinks = document.querySelectorAll('.level-link');
 
+let insertStatsData = (event, data) => {
+  event.preventDefault();
+  let statsObject = data;
+  for (let j = 1; j <= 2; j++) {
+    let stage = 'stage' + j;
+    if (statsObject.average[stage]) {
+      console.log(statsObject.average[stage]);
+      let level = event.target.textContent;
+      if (statsObject.average[stage][level]) {
+        let averageScoreContent = statsObject.average[stage][level].score;
+        console.log(averageScoreContent)
+        averageScore.textContent = `Average score: ${averageScoreContent}`;
+    
+        let averageTimeContent = statsObject.average[stage][level].time;
+        console.log(averageTimeContent)
+        averageTime.textContent = `Average time: ${averageTimeContent}`;
+    
+        let personalScoreContent = statsObject.user[stage][level].score;
+        console.log(personalScoreContent)
+        personalScore.textContent = `Your score: ${personalScoreContent}`;
+    
+        let personalTimeContent = statsObject.user[stage][level].time;
+        console.log(personalTimeContent)
+        personalTime.textContent = `Your time: ${personalTimeContent}`;
+      }
+    }   
+  }
+};
+
+let scoreData
 
 let fetchScores = () => {
   let token = localStorage.getItem("token");
@@ -15,10 +46,19 @@ let fetchScores = () => {
     return res.json();
   })
   .then(data => {
+    scoreData = data;
+    for (levelLink of levelLinks) {
+      levelLink.addEventListener('click', (event) => {
+        insertStatsData(event, data);
+      });
+    }
     showStats();
     console.log(data);
   })
 };
+
+
+
 
 
 
