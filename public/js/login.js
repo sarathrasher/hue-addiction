ce600e25ecfc7aa4b61ee58bb706ed933f6b961f
Sara Muntean <munteansr@gmail.com>
2018-08-31 14:42:40 -0400

    Removed navbar from login screen. Hid create account button on submission

--- public/js/login.js
+++ public/js/login.js
@@ -24,8 +24,7 @@ let createUser = (event) => {
         body: JSON.stringify(data),
   })
   .then(response => {
-    loginForm.reset();
-    showInstructions();
+    createButton.classList.add('hidden');
   })
 }
 
