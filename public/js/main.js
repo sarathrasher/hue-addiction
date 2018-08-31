de69e48c98a8fd1e3406f3845625a961f0095a94
Ian Gornall <ian.gornall@gmail.com>
2018-08-31 11:50:17 -0400

    Data is posted during game!

--- public/js/main.js
+++ public/js/main.js
@@ -18,27 +18,3 @@ let showStats = () => {
 };
 
 statsBtn.addEventListener("click", showStats);
-
-let level_data = {
-  stage: '1',
-  level: '3',
-  score: '11',
-  time: '13'
-};
-console.log(JSON.stringify(level_data));
-let token = localStorage.getItem("token");
-console.log(token);
-fetch('/api/game_data',  {
-  method: 'POST',
-  headers: {
-    "Content-Type": "application/json",
-    "token": token
-  },
-  body: JSON.stringify(level_data)
-}).then(res => {
-  console.log(res);
-  return res.json();
-})
-.then(data => {
-  console.log(data);
-});
