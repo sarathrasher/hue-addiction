eb88eb4606802679ac41561a3f0557c2b215221e
Ian Gornall <ian.gornall@gmail.com>
2018-09-02 10:38:08 -0400

    Added error messages for user when login and registration fails.

--- public/js/login.js
+++ public/js/login.js
@@ -6,6 +6,7 @@
 let createButton = document.querySelector('.create-button');
 let loginButton = document.querySelector('.login-button');
 let loginForm = document.querySelector('.login-form');
+let loginMessage = document.querySelector('.login-message');
 
 let createUser = (event) => {
   event.preventDefault();
@@ -23,8 +24,18 @@ let createUser = (event) => {
         referrer: "no-referrer", 
         body: JSON.stringify(data),
   })
-  .then(response => {
-    createButton.classList.add('hidden');
+  .then(response => response.json())
+  .then(data => {
+    if (data.error) {
+      loginMessage.textContent = "Error.  User already registered.";
+      loginMessage.classList.remove('remove');
+      console.log(data.error);
+    } else {
+      createButton.classList.add('hidden');
+      loginMessage.textContent = "User registered.  You may now login.";
+      loginMessage.classList.remove('remove');
+      console.log('Token created: ' + data.token);
+    }
   })
 }
 
@@ -44,12 +55,23 @@ let loginUser = (event) => {
         referrer: "no-referrer", 
         body: JSON.stringify(data),
   })
-  .then(response => {
-      response.text().then(token => {
-        localStorage.setItem("token", token);
-        loginForm.reset();
-        showInstructions();
-      })
+  .then(response => response.json()).then(data => {
+    if (data.error) {
+      loginMessage.textContent = "User could not login.  Check password.";
+      loginMessage.classList.remove('remove');
+      console.log(data.error);
+    } else {
+      localStorage.setItem("token", data.token);
+      loginForm.reset();
+      showInstructions();
+      loginMessage.classList.add('remove');
+    }
+  })
+  .catch(err => {
+    loginMessage.textContent = "Error accessing server.";
+    loginForm.reset();
+    loginMessage.classList.remove('remove');
+    console.log(err);
   })
 }
 
